2d156f1459e639bf7850a36aa0b5f269cf932edf
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-05-13 10:53:57 +0300

    ygvyhgv

---
+++ testing_xcode/testing_xcode/testing_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_xcodeApp.swift
+//  testing_xcode
+//
+//  Created by Abdullah Hafiz on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct testing_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
